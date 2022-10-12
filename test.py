bb6e7159ced0891c496a2548257922b7f8f830c7
poijection1 <89765467+poijection1@users.noreply.github.com>
2022-10-12 16:49:36 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+import requests
+
+print('started')
+url = f'https://web.archive.org/cdx/search/cdx?url=www.trendyol.com/*&output=json&collapse=urlkey&fl=original&page=0'
+headers = {
+'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0',
+'Accept-Language' : 'en-US,en;q=0.5',
+'Accept-Encoding' : 'gzip, deflate, br'
+}
+resp = requests.get(url, headers=headers, timeout=15)
+print(resp.status_code)
